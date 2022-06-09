3573b97f62bb8fc024edade9e781a7dd5f6c287a
Agustin-Grande <agusgrandedx@gmail.com>
2022-06-09 18:36:12 -0300

    fecha y hora

--- main.js
+++ main.js
@@ -45,6 +45,12 @@ function Calcular(){
   
   }
 
+
+  let date = new Date();
+
+  let output = String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '/' + date.getFullYear() + ' ' + date.getHours();
+  console.log(date);
+
 }
 function Imprimir(){
     
